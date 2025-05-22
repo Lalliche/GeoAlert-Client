e435922b034b7af1f682ab946901bea8d747b38d
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-22 16:49:02 +0100

    Update notificationApi.js

--- src/api/notificationApi.js
+++ src/api/notificationApi.js
@@ -13,6 +13,8 @@ export const impactedUsers = async (alertId) => {
       },
     });
 
+    console.log("response in impacted user from server is", response); // Debugging line
+
     return response.data;
   } catch (error) {
     console.error("Error fetching impacted users:", error);
