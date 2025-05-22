d9e1de36f7469572d249f7ca32fa7ccde187b155
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-22 16:49:10 +0100

    Update zonesApi.js

--- src/api/zonesApi.js
+++ src/api/zonesApi.js
@@ -2,12 +2,12 @@
 import axios from "./geoBase";
 import Cookies from "js-cookie";
 
+const accessToken = Cookies.get("access");
+
 export const addZone = async (name, coordinates) => {
   console.log("Adding zone with name:", name, "and coordinates:", coordinates); // ⬅️ Debugging line
 
   try {
-    const accessToken = Cookies.get("access"); // ⬅️ Get the token from cookies
-
     const response = await axios.post(
       "/addZone",
       { name, coordinates },
@@ -26,23 +26,17 @@ export const addZone = async (name, coordinates) => {
   }
 };
 
-export const deleteZone = async (name) => {
-  console.log("Deleting zone with name:", name); // ⬅️ Debugging line
+export const deleteZone = async (id) => {
+  console.log("Deleting zone with id:", id); // ⬅️ Debugging line
   try {
-    const accessToken = Cookies.get("access");
+    const response = await axios.delete(`/deleteZone/${id}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-    const response = await axios.patch(
-      "/deleteZone",
-      { name: name },
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
     console.log("Zone deleted debug:", response); // ⬅️ Debugging line
-
     return response.data;
   } catch (error) {
     console.error("Error deleting zone:", error);
@@ -52,8 +46,6 @@ export const deleteZone = async (name) => {
 
 export const updateFrequency = async (name, frequency) => {
   try {
-    const accessToken = Cookies.get("access");
-
     const response = await axios.post(
       "/updateFrequency",
       { name, frequency },
@@ -72,10 +64,28 @@ export const updateFrequency = async (name, frequency) => {
   }
 };
 
+export const getZones = async () => {
+  try {
+    const response = await axios.get(
+      "/Zones",
+
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json", // ⬅️ Add Bearer token
+        },
+      }
+    );
+
+    return response.data; // returns the list of zones
+  } catch (error) {
+    console.error("Error fetching zones:", error);
+    throw error; // rethrow for the caller to handle
+  }
+};
+
 export const getAllZones = async () => {
   try {
-    const accessToken = Cookies.get("access"); // ⬅️ Get the token from cookies
-
     const response = await axios.get(
       "/AllZones",
 
@@ -96,8 +106,6 @@ export const getAllZones = async () => {
 
 export const getWilayaByName = async (wilayaName) => {
   try {
-    const accessToken = Cookies.get("access");
-
     const response = await axios.get(
       `/wilaya/${encodeURIComponent(wilayaName)}`,
       {
@@ -125,8 +133,6 @@ export const getUserTracking = async (id, duration) => {
     duration
   ); // Debugging line
   try {
-    const accessToken = Cookies.get("access");
-
     const response = await axios.get(`/tracking/${id}`, {
       params: { duration },
       headers: {
@@ -139,6 +145,48 @@ export const getUserTracking = async (id, duration) => {
     return response.data;
   } catch (error) {
     console.error(`Error fetching tracking data for user ${id}:`, error);
+    throw error;
+  }
+};
+
+export const getFrequency = async () => {
+  try {
+    const response = await axios.get(
+      `/AllFrequencies`,
+
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("Frequency data debug:", response.data); // Debugging line
+
+    return response.data; // returns the frequency data
+  } catch (error) {
+    console.error(`Error fetching frequency for '${name}':`, error);
+    throw error;
+  }
+};
+
+export const getUserInZone = async (zoneName) => {
+  console.log("Fetching users in zone:", zoneName); // Debugging line
+  try {
+    const encodedZoneName = encodeURIComponent(zoneName);
+
+    const response = await axios.get(`/GetUserInZone/${encodedZoneName}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Fetched users in zone:", response.data); // Debugging line
+    return response.data; // [{ UserId: "2" }, ...]
+  } catch (error) {
+    console.error(`Error fetching users in zone ${zoneName}:`, error);
     throw error;
   }
 };
