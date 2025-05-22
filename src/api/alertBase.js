2cbff5059b62e44b42b2cd856524665f9fde7f96
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-22 16:48:45 +0100

    Update alertBase.js

--- src/api/alertBase.js
+++ src/api/alertBase.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const authBase = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_ALERT,
+const alertBase = axios.create({
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_DOMAIN_LOCAL +
+    process.env.NEXT_PUBLIC_API_BASE_URL_ALERT,
 });
 
-export default authBase;
+export default alertBase;
