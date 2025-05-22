7ca59c458240779df4df8352a97855bccebd1e21
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-22 16:48:54 +0100

    Update authBase.js

--- src/api/authBase.js
+++ src/api/authBase.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const authBase = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_AUTH,
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_DOMAIN_LOCAL +
+    process.env.NEXT_PUBLIC_API_BASE_URL_AUTH,
 });
 
 export default authBase;
