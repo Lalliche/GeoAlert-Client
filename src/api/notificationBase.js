a42e92e8a79ac80a5c6e9661c32c76787630a230
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-22 16:49:06 +0100

    Update notificationBase.js

--- src/api/notificationBase.js
+++ src/api/notificationBase.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const noticationBase = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_NOTIFICATION,
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_DOMAIN_LOCAL +
+    process.env.NEXT_PUBLIC_API_BASE_URL_NOTIFICATION,
 });
 
 export default noticationBase;
