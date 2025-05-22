7ab96d35074bec006d062a854dfd1c67c973224d
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-22 16:48:58 +0100

    Update geoBase.js

--- src/api/geoBase.js
+++ src/api/geoBase.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const geoBase = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_GEO,
+  baseURL:
+    process.env.NEXT_PUBLIC_APP_DOMAIN_LOCAL +
+    process.env.NEXT_PUBLIC_API_BASE_URL_GEO,
 });
 
 export default geoBase;
