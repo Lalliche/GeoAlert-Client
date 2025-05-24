257f10f5b349fc8424f1d2dcc49d59438c171721
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-24 07:44:56 +0100

    Update SideBar component

--- src/Components/Global/SideBar.jsx
+++ src/Components/Global/SideBar.jsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import Spinner from "./Spinner";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const AdminRoutes = [
   {
@@ -32,9 +33,16 @@ const AdminRoutes = [
   {
     name: "Zones Management",
     Icon: TbMap2,
-    path: "/dashboard/zones", // used for isActive check
-    opt: "/dashboard/zones?map", // used for navigation
+    path: "/dashboard/zones",
+    opt: "/dashboard/zones?map",
     pathName: "Zones",
+    isPartialMatch: true,
+  },
+  {
+    name: "Settings",
+    Icon: IoSettingsOutline,
+    path: "/dashboard/settings",
+    pathName: "Settings",
     isPartialMatch: true,
   },
 ];
@@ -42,16 +50,6 @@ const AdminRoutes = [
 const SideBar = () => {
   const router = useRouter();
   const pathname = usePathname();
-  const [role, setRole] = useState("Admin");
-
-  /*  useEffect(() => {
-    const userRole = Cookies.get("role");
-    if (userRole !== "Admin") {
-      router.replace("/login"); 
-    } else {
-      setRole(userRole);
-    }
-  }, []); */
 
   const handleLogout = () => {
     Cookies.remove("role");
@@ -68,34 +66,28 @@ const SideBar = () => {
       </p>
 
       <div className="w-full justify-between flex flex-col h-full">
-        {!role ? (
-          <div className="w-full h-full center">
-            <Spinner />
-          </div>
-        ) : (
-          <div className="flex flex-col gap-[1.75em] ">
-            {AdminRoutes.map((route, index) => {
-              const isActive = route.isPartialMatch
-                ? pathname.startsWith(route.path)
-                : pathname === route.path;
+        <div className="flex flex-col gap-[1.75em] ">
+          {AdminRoutes.map((route, index) => {
+            const isActive = route.isPartialMatch
+              ? pathname.startsWith(route.path)
+              : pathname === route.path;
 
-              return (
-                <Link
-                  href={route.opt || route.path}
-                  key={index}
-                  className={`flex flex-row font-semibold items-center gap-[1.5em] py-[0.5em] px-[0.75em] rounded-sm transition-all font-titillium-web ${
-                    isActive
-                      ? "bg-[#FFF5F5] text-main border-l-[3px] border-main"
-                      : "text-[#7A7A7A] hover:text-[#DC091A] hover:bg-[#F5F5F5]"
-                  }`}
-                >
-                  <route.Icon className="text-[1.8em]" />
-                  <span>{route.name}</span>
-                </Link>
-              );
-            })}
-          </div>
-        )}
+            return (
+              <Link
+                href={route.opt || route.path}
+                key={index}
+                className={`flex flex-row font-semibold items-center gap-[1.5em] py-[0.5em] px-[0.75em] rounded-sm transition-all font-titillium-web ${
+                  isActive
+                    ? "bg-[#FFF5F5] text-main border-l-[3px] border-main"
+                    : "text-[#7A7A7A] hover:text-[#DC091A] hover:bg-[#F5F5F5]"
+                }`}
+              >
+                <route.Icon className="text-[1.8em]" />
+                <span>{route.name}</span>
+              </Link>
+            );
+          })}
+        </div>
 
         {/* Logout Button */}
         <div
