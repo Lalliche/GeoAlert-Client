174731ce564aa81598c510ef47eb16e2a8b52562
Lalliche <abdelhadi.lalliche@gmail.com>
2025-05-26 01:19:36 +0100

    Update overview page

--- src/app/dashboard/overview/page.jsx
+++ src/app/dashboard/overview/page.jsx
@@ -1,25 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import StatusMessage from "@/Components/Global/StatusMessage";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const PieChartComponent = dynamic(
   () => import("@/Components/Statistics/PieChart"),
   { ssr: false }
 );
 
-const AlertAnalytics = dynamic(
-  () => import("@/Components/Global/AlertAnalytics"),
-  { ssr: false }
-);
+const AppUsers = dynamic(() => import("@/Components/Global/AppUsers"), {
+  ssr: false,
+});
 
 const OverviewPage = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="w-full col gap-[1em] px-[2em] pb-[5em]">
+    <div className="w-full flex flex-col gap-[1em] px-[2em] pb-[5em]">
       <StatusMessage
         error={error}
         success={success}
@@ -29,10 +37,21 @@ const OverviewPage = () => {
           setSuccess(null);
         }}
       />
-      <h1 className="text-2xl font-bold mb-4">Overview</h1>
-      <PieChartComponent error={setError} loading={setLoading} />
-      <div className="w-full">
-        <AlertAnalytics />
+
+      {/* AppUsers */}
+      <div>
+        {loading ? <Skeleton height={300} className="rounded-[0.6em]" /> : null}
+        <div style={{ display: loading ? "none" : "block" }}>
+          <AppUsers />
+        </div>
+      </div>
+
+      {/* PieChart */}
+      <div>
+        {loading ? <Skeleton height={300} className="rounded-[0.6em]" /> : null}
+        <div className="w-1/2" style={{ display: loading ? "none" : "block" }}>
+          <PieChartComponent error={setError} loading={setLoading} />
+        </div>
       </div>
     </div>
   );
